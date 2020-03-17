742cf09c564fc60ec6ca4ba2d6e6d2483597eb75
Ron (rroonniinn) <g.mogilewski@gmail.com>
2020-03-17 18:06:13 +0100

    Dodana funkcja copySheet

---
+++ v02/gas/copySheetsToOther.js
@@ -0,0 +1,37 @@
+// @ts-nocheck
+import { pipe } from '../fp/pipe';
+
+/**
+ * Kopiuje arkusze które pasują do przekazanego callbacku z jednego
+ * arkusza do drugiego. Nazwy pozostają takie same, chyba że już istnieje
+ * taki arkusz - wtedy dokleja info 'Copy'
+ *
+ * @param {string} sourceId Id pliku źródłowego
+ * @param {string} targetId Id pliku docelowego
+ * @param {Function} callback np. sheet => sheet.getName().includes('res')
+ */
+
+const copySheetsToOther = (sourceId, targetId, callback) => {
+	const targetSpreadsheet = SpreadsheetApp.openById(targetId);
+	const existing = targetSpreadsheet
+		.getSheets()
+		.map(sheet => sheet.getName());
+
+	const setName = sheet => {
+		const orgName = sheet.getName();
+		return existing.includes(orgName) ? `Copy ${orgName}` : orgName;
+	};
+
+	pipe(
+		() =>
+			SpreadsheetApp.openById(sourceId)
+				.getSheets()
+				.filter(callback),
+		arr =>
+			arr.forEach(sheet =>
+				sheet.copyTo(targetSpreadsheet).setName(setName(sheet))
+			)
+	)();
+};
+
+export { copySheetsToOther };
