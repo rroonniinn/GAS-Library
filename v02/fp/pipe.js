1fdac888518fc43b313963624e001a24d616fc80
Ron (rroonniinn) <g.mogilewski@gmail.com>
2020-03-19 12:53:02 +0100

    Dodany getJson

--- v02/fp/pipe.js
+++ v02/fp/pipe.js
@@ -2,11 +2,10 @@
  * Własna implementacja pipe oparta na materiale z
  * // https://medium.com/javascript-scene/reduce-composing-software-fe22f0c39a1d
  *
- * @param {function}  fns Dowolna liczna funkcji
- * @returns {(x:any) => any} Zwraca funkcję oczekującą wartości inicjalnej
+ * @param {any}  fns Dowolna liczna funkcji
+ * @returns {function} Zwraca funkcję oczekującą wartości inicjalnej
  */
 
-const pipe = (/** @type {array} */ ...fns) => x =>
-	fns.reduce((v, f) => f(v), x);
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
 
 export { pipe };
---
+++ v02/str/getJson.js
@@ -0,0 +1,35 @@
+/* eslint-disable max-params */
+import { pipe } from '../fp/pipe';
+import { getSheet } from '../gas/getSheet';
+import { tail } from '../../v01/arr/tail';
+import { getValues } from '../../v01/gas/getValues';
+import { moveFiles } from '../../v01/gas/moveFiles';
+
+/**
+ * Zapisuje na Drive plik JSON z danymi ze wskazanego arkusza
+ *
+ * @param {string} fileName Docelowa nazwa pliku json
+ * @param {string} folderId Id folderu w którym ten plik ma wylądować
+ * @param {string} srcSheetName Nazwa arkusza z danymi
+ * @param {string} [srcId] Opcjonalne id pliku
+ */
+
+const getDataAsJson = (fileName, folderId, srcSheetName, srcId) => {
+	const sheet = getSheet(srcSheetName, srcId);
+	pipe(
+		() => sheet,
+		getValues,
+		tail,
+		arr => JSON.stringify(arr),
+		str =>
+			DriveApp.createFile(
+				`${fileName}.json`,
+				str,
+				// @ts-ignore
+				MimeType.PLAIN_TEXT
+			),
+		file => moveFiles([file], folderId)
+	)();
+};
+
+export { getDataAsJson };
