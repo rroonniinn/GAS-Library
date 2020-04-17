6359f3cfea2a71432d25298ee0ed9b5fd9ddfd47
Ron (rroonniinn) <g.mogilewski@gmail.com>
2020-04-17 09:26:39 +0200

    Drobne

--- v02/experiments/manualTesting/buildExternals.js
+++ v02/experiments/manualTesting/buildExternals.js
@@ -1,13 +1,14 @@
 import { buildExternals as fn } from '../buildExternals';
-import { EXP_SETUP } from './config';
 import { getFolder } from '../../gas/getFolder';
+
+import { EXP_SETUP } from './config';
 
 const url =
 	'https://drive.google.com/drive/folders/1g7wQ77tKi_ErC47qWkYNoUo5rk4yzJpa';
-const folder = getFolder(url);
+const folder = () => getFolder(url);
 
 const buildExternals = {
-	a1: () => fn(folder)(EXP_SETUP),
+	a1: () => fn(folder())(EXP_SETUP),
 };
 
 export { buildExternals };
--- v02/experiments/manualTesting/buildHub.js
+++ v02/experiments/manualTesting/buildHub.js
@@ -1,13 +1,14 @@
 import { buildHub as fn } from '../buildHub';
-import { EXP_SETUP } from './config';
 import { getFolder } from '../../gas/getFolder';
+
+import { EXP_SETUP } from './config';
 
 const url =
 	'https://drive.google.com/drive/folders/1auxE9U0gpQOaU5oyobzFitiCA6zFWaHS';
-const folder = getFolder(url);
+const folder = () => getFolder(url);
 
 const buildHub = {
-	a1: () => fn(folder)(EXP_SETUP),
+	a1: () => fn(folder())(EXP_SETUP),
 };
 
 export { buildHub };
