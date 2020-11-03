f899f84806cf3226ded96f258431a04e1d9c6c63
Ron (rroonniinn) <g.mogilewski@gmail.com>
2020-11-03 14:09:05 +0100

    added showSidebar

---
+++ v02/gas/showSidebar.js
@@ -0,0 +1,17 @@
+/**
+ * Odpalanie sidebaru
+ * @param {string} srcHtml - nazwa pliku html do wyświetlenia w sidebarze np. 'index.html'
+ * @param {string} title - wyświetlany tytuł sidebaru
+ * @return {void}
+ */
+
+const showSidebar = (srcHtml, title) => {
+	const ui = HtmlService.createTemplateFromFile(srcHtml)
+		.evaluate()
+		.setSandboxMode(HtmlService.SandboxMode.IFRAME)
+		.setTitle(title);
+
+	SpreadsheetApp.getUi().showSidebar(ui);
+};
+
+export {showSidebar}
