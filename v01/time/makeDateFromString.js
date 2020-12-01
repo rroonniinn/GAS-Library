1c73d3c8de47614150fbe9bbf66eace2ce8d9a22
Ron (rroonniinn) <g.mogilewski@gmail.com>
2020-12-01 09:29:50 +0100

    minor update makeDateFromString

--- v01/time/makeDateFromString.js
+++ v01/time/makeDateFromString.js
@@ -1,13 +1,13 @@
 /**
- * Zwraca poprawną datę dla stringa typu '2010-01-01'
+ * Zwraca poprawną datę dla ciągu typu '2010-01-01'
  *
  * @param {string} str String typu '2010-01-01' lub '2010.01.01'
- * @returns {string} Poprawny obikt daty
+ * @returns {Date} Poprawny obiekt daty
  */
 const makeDateFromString = str => {
-	const year = /^([0-9]+)\D/.exec(str)[1];
-	const month = /\D([0-9]+)\D/.exec(str)[1];
-	const day = /\D([0-9]+)$/.exec(str)[1];
+	const year = Number(/^([0-9]+)\D/.exec(str)[1]);
+	const month = Number(/\D([0-9]+)\D/.exec(str)[1]);
+	const day = Number(/\D([0-9]+)$/.exec(str)[1]);
 	return new Date(year, month - 1, day);
 };
 
