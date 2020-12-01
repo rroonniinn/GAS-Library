96e850ded5fd2c6e009e4c19cc4945010ac3a5f3
Ron (rroonniinn) <g.mogilewski@gmail.com>
2020-12-01 15:25:44 +0100

    added restoreDateFromJson

---
+++ v02/json/restoreDateFromJsonObj.js
@@ -0,0 +1,18 @@
+import { isDateStr } from './../../v01/time/isDateStr';
+
+/**
+ * Przyjmując obiekt post JSON-owy zawierający m.in. stringi daty,
+ * zamienia je na normalne daty. Pozostałych wartości nie zmienia
+ * @param {Object<string, any>} postJsonObj
+ */
+
+const restoreDateFromJsonObj = postJsonObj =>
+	Object.entries(postJsonObj)
+		.map(([key, val]) => [key, isDateStr(val) ? new Date(val) : val])
+		.reduce((res, el) => {
+			const [key, val] = el;
+			res[key] = val;
+			return res;
+		}, {});
+
+		export { restoreDateFromJsonObj }
