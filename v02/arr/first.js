36010c2689dfdbf06ad5ee2ac336e67527b5ff01
Ron (rroonniinn) <g.mogilewski@gmail.com>
2020-12-11 12:43:59 +0100

    first and last added

---
+++ v02/arr/first.js
@@ -0,0 +1,6 @@
+/**
+ * Zwraca pierwszy element tablicy
+ * Dla tablicy pustej zwraca undefined
+ * @param {array} arr
+ */
+export const first = arr => arr[0];
---
+++ v02/arr/last.js
@@ -0,0 +1,6 @@
+/**
+ * Zwraca ostatni element tablicy.
+ * Dla tablicy pustej zwraca undefined
+ * @param {array} arr
+ */
+export const last = arr => arr[arr.length - 1];
