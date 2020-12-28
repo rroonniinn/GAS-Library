b56e41142f6f74e1101fe6e990382b7d364bf3e6
Ron (rroonniinn) <g.mogilewski@gmail.com>
2020-12-28 10:52:53 +0100

    update

--- v02/utils/isArray2d.js
+++ v02/utils/isArray2d.js
@@ -1,4 +1,21 @@
 /**
+Przetestowane na:
+
+var a = '1'
+var b = []
+var c = [[]]
+var d = [1,2,[1]]
+var e = [[1,2,3], [1,2,3]]
+
+console.log('a', isArray2d(a) === false)	// ->  true
+console.log('b',  isArray2d(b) === false)	// ->  true
+console.log('c', isArray2d(c) === true)		// ->  true
+console.log('d', isArray2d(d) === false)	// ->  true
+console.log('e', isArray2d(e) === true)		// ->  true
+
+ */
+
+/**
  * Weryfikuje czy przekazana wartość jest tablicą 2d
  * @param {*} val
  * @returns {boolean}
