2fbb1107f5248a083f6eeaa1644084b5580ffeb2
Ron (rroonniinn) <g.mogilewski@gmail.com>
2020-12-29 19:58:05 +0100

    getDaysInMonth update

--- v01/time/getDaysInMonth.js
+++ v01/time/getDaysInMonth.js
@@ -4,7 +4,7 @@ import { isDate } from '../../v02/utils/isDate';
  * Zwraca liczbę dni w miesiącu z danego roku
  * month, year strings / month - 1 dla stycznia, 2 dla lutego itp.
  * @param {number|Date} yearDate rok lub obiekt daty
- * @param {number} month
+ * @param {number} [month]
  * @returns {number} Liczba dni we wskazanym miesiącu
  */
 
