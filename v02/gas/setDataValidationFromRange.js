c3c0ff9f75cc107121343aa3fbbbe40e348970b8
Ron (rroonniinn) <g.mogilewski@gmail.com>
2021-01-04 20:43:16 +0100

    setDataValidationFromRange - dodana opcja nie przyjmowania invalid

--- v02/gas/setDataValidationFromRange.js
+++ v02/gas/setDataValidationFromRange.js
@@ -4,16 +4,20 @@
  * @param {GoogleAppsScript.Spreadsheet.Sheet} rulesSheet Obiekt arkusza w którym znajdują się reguły
  * @param {string} rulesRange Zakres w którym znajdują się reguły (jako A1 Notation)
  * @param {GoogleAppsScript.Spreadsheet.Range} rangeToApply Range do którego należy zaaplikować data validation
+ * @param {GoogleAppsScript.Spreadsheet.Range} rangeToApply Range do którego należy zaaplikować data validation
+ * @param {boolean} [allowInvalid] Czy pozwolić na nieprawiłowe dane (pojawi się trójkąt) czy nie.
  */
 const setDataValidationFromRange = (
 	rulesSheet,
 	rulesRange,
-	rangeToApply
+	rangeToApply,
+	allowInvalid = true
 ) => {
 	const rangeForValidation = rulesSheet.getRange(rulesRange);
 
 	const rule = SpreadsheetApp.newDataValidation()
 		.requireValueInRange(rangeForValidation)
+		.setAllowInvalid(allowInvalid)
 		.build();
 
 	rangeToApply.setDataValidation(rule);
