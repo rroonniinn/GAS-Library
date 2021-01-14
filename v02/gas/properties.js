6577117b3362161d4443084adb9fbf18cabc1adf
Ron (rroonniinn) <g.mogilewski@gmail.com>
2021-01-14 10:31:31 +0100

    dodana metoda do propertis - del

--- v02/gas/properties.js
+++ v02/gas/properties.js
@@ -5,8 +5,9 @@ import { CrusherPluginPropertyService } from '../gas-mcpher/CrusherPluginPropert
  * Cache Object
  * @typedef {Object} CacheObject
  * @property {object} init Metoda do inicjacji obiektu (nie używana pub)
- * @property {function} get Pobieranie danych z cacha
- * @property {function} put Wprowadzanie danych do cacha
+ * @property {function} get Pobieranie danych z propsa
+ * @property {function} put Wprowadzanie danych do propsa
+ * @property {function} del Usuwa dane z propsa
  */
 /**
  * Obiekt do obsługi cachowania z biblioteki mcpher
@@ -23,7 +24,7 @@ const crusherProps = {
 	 */
 	get(key) {
 		console.log(`Pobieram z Propsów cr-${key}`);
-		return this.init.get(`cr-${key}`);
+		return this.init.get(`pr-${key}`);
 	},
 
 	/**
@@ -31,12 +32,21 @@ const crusherProps = {
 	 * @param {any} vals Dane do wprowadzenia
 	 */
 	put(key, vals) {
-		this.init.put(`cr-${key}`, vals);
+		this.init.put(`pr-${key}`, vals);
+	},
+
+	/**
+	 * @param {string} key Klucz danych z cacha
+	 * @param {any} vals Dane do wprowadzenia
+	 */
+	del(key) {
+		this.init.remove(`pr-${key}`);
 	},
 };
 
 const getProps = propName => crusherProps.get(propName);
 const setProps = (propName, val) => crusherProps.put(propName, val);
+const delProps = (propName) => crusherProps.del(propName);
 
 // crusherProps jest exportowany tylko z powodu legacy code
-export { crusherProps, getProps, setProps };
+export { crusherProps, getProps, setProps, delProps };
