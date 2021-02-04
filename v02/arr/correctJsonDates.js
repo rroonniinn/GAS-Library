87512c0f4cf564b7dbb49463fe4135bbc48e87bd
Ron (rroonniinn) <g.mogilewski@gmail.com>
2021-02-04 12:32:18 +0100

    added correctJsonDates

---
+++ v02/arr/correctJsonDates.js
@@ -0,0 +1,22 @@
+/**
+ * Poprawia daty w tablicach uzyskanych z JSON.parse.
+ * Przywraca daty jako obiekty. Przyjmuje tablice 1d i 2d.
+ * Zwraca nową kopię tablicy
+ * @param {array} postJsonArr
+ * @returns {array}
+ */
+
+const correctJsonDates = postJsonArr => {
+	const isJsonDate = v => /\d{4}-\d{2}-\d{2}T/.test(v);
+	return postJsonArr.map(el => {
+		if (Array.isArray(el)) {
+			return el.map(cell =>
+				isJsonDate(cell) ? new Date(cell) : cell
+			);
+		}
+
+		return isJsonDate(el) ? new Date(el) : el;
+	});
+};
+
+export { correctJsonDates };
