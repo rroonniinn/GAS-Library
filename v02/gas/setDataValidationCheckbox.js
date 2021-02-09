c11e14a76bdd67e1c5e806aa763255881d0fd121
Ron (rroonniinn) <g.mogilewski@gmail.com>
2021-02-09 20:03:12 +0100

    minor fixes to setDataValidationCheckbox

--- v02/gas/setDataValidationCheckbox.js
+++ v02/gas/setDataValidationCheckbox.js
@@ -1,14 +1,15 @@
 /**
  * Ustawia data validation na checkbox
- * @param {GoogleAppsScript.Spreadsheet.Range} rangeToApply Range do którego należy zaaplikować data validation
+ * @param {GoogleAppsScript.Spreadsheet.Range[]} rangesToApply Range do którego należy zaaplikować data validation
  */
 
-const setDataValidationCheckbox = rangeToApply => {
+const setDataValidationCheckbox = rangesToApply => {
 	const rule = SpreadsheetApp.newDataValidation()
 		.requireCheckbox()
 		.build();
 
-	rangeToApply.setDataValidation(rule);
+		rangesToApply.forEach(range => range.setDataValidation(rule))
+
 };
 
 export { setDataValidationCheckbox };
