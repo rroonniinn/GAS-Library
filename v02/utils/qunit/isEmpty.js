7bf53de5a9986deff858177a4fb725727558a282
Ron (rroonniinn) <g.mogilewski@gmail.com>
2021-02-18 11:29:42 +0100

    unitTesty

---
+++ v02/utils/qunit/isEmpty.js
@@ -0,0 +1,24 @@
+// @ts-nocheck
+/* global QUnit, strictEqual */
+
+import { isEmpty as fn } from '../isEmpty';
+
+const title = 'isEmpty | arrays | Testy na tablicach';
+
+const msg = {
+	t01: '[] - tak',
+	t02: '[[]] - tak',
+};
+
+const input = {
+	a01: [],
+	a02: [[]],
+};
+
+const arrays = () =>
+	QUnit.test(title, () => {
+		strictEqual(fn(input.a01), true, msg.t01);
+		strictEqual(fn(input.a02), true, msg.t02);
+	});
+
+export { arrays };
