c51ac0214f0258d804ec8e26a4d582cc52379e44
Ron (rroonniinn) <g.mogilewski@gmail.com>
2021-03-09 11:29:50 +0100

    added setDataValidationFromList

--- v02/gas/setDataValidationFromList.js
+++ v02/gas/setDataValidationFromList.js
@@ -1,26 +1,22 @@
-/* eslint-disable max-params */
 /**
  * Ustawia data validation na listę wartości znajdujących się
  * w arkuszu (tym samym lub innym)
- * @param {GoogleAppsScript.Spreadsheet.Sheet} rulesSheet Obiekt arkusza w którym znajdują się reguły
- * @param {string} rulesRange Zakres w którym znajdują się reguły (jako A1 Notation)
+ * @param {array} valueList Tablica z dozwolonymi wartościami
  * @param {GoogleAppsScript.Spreadsheet.Range[]} rangesToApply Range do którego należy zaaplikować data validation
  * @param {boolean} [allowInvalid] Czy pozwolić na nieprawidłowe dane (pojawi się trójkąt) czy nie.
  */
-const setDataValidationFromRange = (
-	rulesSheet,
-	rulesRange,
+
+const setDataValidationFromList = (
+	valueList,
 	rangesToApply,
 	allowInvalid = true
 ) => {
-	const rangeForValidation = rulesSheet.getRange(rulesRange);
-
 	const rule = SpreadsheetApp.newDataValidation()
-		.requireValueInRange(rangeForValidation)
+		.requireValueInList(valueList)
 		.setAllowInvalid(allowInvalid)
 		.build();
 
 	rangesToApply.forEach(range => range.setDataValidation(rule));
 };
 
-export { setDataValidationFromRange };
+export { setDataValidationFromList };
