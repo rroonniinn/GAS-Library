0d3e8652d694263af626191d3961afbae5fed6f3
Ron (rroonniinn) <g.mogilewski@gmail.com>
2021-03-10 10:34:40 +0100

    jsdocs updates areValuesUnique

--- v02/arr/areValuesUnique.js
+++ v02/arr/areValuesUnique.js
@@ -1,6 +1,15 @@
+
+/**
+ * @module Arr
+ */
+
 /**
  * Sprawdza czy w przekazanej tablicy nie ma duplikatów.
  * Przed sprawdzeniem usuwa puste wartości
+ * @instance
+ * @function areValuesUnique
+ * @param {array} arr Tablica 1d z wartościami
+ * @returns {boolean}
  */
 
 export const areValuesUnique = arr => {
