de4cb191f9f8ac392300655f83bf7018475abb6c
Ron (rroonniinn) <g.mogilewski@gmail.com>
2021-03-31 15:53:28 +0200

    updates

--- v02/num/randomInteger.js
+++ v02/num/randomInteger.js
@@ -1,11 +1,14 @@
 /**
- * Returns (pseudo) random integer from min - max range (inclusive)
+ * Returns (pseudo) random integer from min - max range.
+ * Min and max included
+ * Taken from https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
  *
  * @param {number} min Min value (inclusive)
  * @param {number} max Max value (inclusive)
+ * @returns {number} Pseudo random number
  */
 
 const randomInteger = (min, max) =>
 	Math.floor(Math.random() * (max - min + 1) + min);
 
-export { randomInteger };
+export { randomInteger, randomInteger as getRandomInt };
--- v02/time/getLastDay.js
+++ v02/time/getLastDay.js
@@ -19,3 +19,7 @@ const getLastDay = date =>
 	new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
 export { getLastDay };
+
+/**
+ * Ut - done
+ */
