12496402e52705734bb1e70d71cf9215a52fb2a5
Ron (rroonniinn) <g.mogilewski@gmail.com>
2021-07-21 22:17:40 +0200

    drobne

--- v01/gas/activateSheet.js
+++ v01/gas/activateSheet.js
@@ -2,6 +2,7 @@
  * Aktywuje arkusza o przekazanej nazwie.
  * Tym samy przełącza na niego interfejs
  * @param {string} sheetName
+ * @returns {GoogleAppsScript.Spreadsheet.Sheet}
  */
 
 export const activateSheet = sheetName =>
