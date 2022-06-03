8bb30cd61ce0664d2528e3ff1ef8002e368b1f4a
Ron (rroonniinn) <g.mogilewski@gmail.com>
2022-06-03 17:47:32 +0200

    update

--- v01/dom/addGaTrackingUniversal.js
+++ v01/dom/addGaTrackingUniversal.js
@@ -1,26 +1,41 @@
 /**
  * Dodaje event na kliknięcie wysyłający do GA info o evencie
  * @param {string} location Ścieżka 'strony' - np. 'budget_listing/budget'
- * @param {string} ssId Id skoroszytu - funkcjonuje jako id usera
+ * @param {string} userId Id usera
  * @param {string} sysVer Wersja BudgetStudio
+ * @param {string} ssId Id skoroszytu - funkcjonuje jako id usera
+ * @param {string} dbType Typ bazy np. "Baza_Demo"
  */
 
-export const addGaTrackingUniversal = (location, ssId, sysVer) => () => {
+export const addGaTrackingUniversal = (
+	location,
+	userId,
+	sysVer,
+	ssId,
+	dbType
+) => () => {
 	const consts = {
 		url: 'https://www.google-analytics.com/',
 		type: 'collect?v=1',
 	};
 
+	const dtString = `v:${sysVer} ss:${ssId} db:${dbType}`;
+
 	const events = {
 		tid: 'UA-221769883-2',
-		cid: ssId,
-		uid: ssId,
-		cn: sysVer,
+		cid: userId,
+		uid: userId,
+		fl: sysVer,
+		// cn: sysVer,
+		dt: dtString,
 		cs: 'version',
-		cm: sysVer,
+		// cm: sysVer,
+		// av: sysVer,
 		t: 'pageview',
 		dp: location,
 	};
+
+	// console.log('events', events);
 
 	const strfds = Object.entries(events)
 		.map(([opts, val]) => `${opts}=${val}`)
